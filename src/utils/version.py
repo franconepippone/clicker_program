eb0df4a764ceef65ca691cce3d3c45851d31ae3b
Pippone Francone <americo.cherubini2003@gmail.com>
2025-11-11 21:03:06 +0100

    Update version.py
    no longer reading version from .toml file, as that causes problems on frozen app

--- src/utils/version.py
+++ src/utils/version.py
@@ -1,11 +1,16 @@
 import tomllib
 from packaging.version import Version, InvalidVersion
+from utils.resource_resolver import resource_path
 
+__version__ = "1.0.3"
+
+def get_pyproject_version_str() -> str:
+    return __version__
 
 def get_pyproject_version() -> Version | None:
     """Reads version from pyproject.toml file
     """
-    with open("pyproject.toml", "rb") as f:
+    with open(resource_path("pyproject.toml"), "rb") as f:
         data = tomllib.load(f)
 
     version_str: str = data["project"]["version"]
@@ -15,7 +20,7 @@ def get_pyproject_version() -> Version | None:
         return None
 
 
-def get_pyproject_version_str(invalid: str = "invalid version") -> str:
+def _get_pyproject_version_str(invalid: str = "invalid version") -> str:
     ver = get_pyproject_version()
     if ver:
         return str(ver)
